86d6c26574a8d0b6f42bb03c5f08c3a3905710fa
Luke <luchenqun@qq.com>
2024-10-18 18:43:05 +0800

    add akkad network

---
+++ README.md
@@ -0,0 +1,5 @@
+# docker-compose scan for akkad
+
+* sh deploy start 启动服务
+* sh deploy stop 停止服务
+* sh deploy reset 清空数据并重启服务，适用于当链数据已经被重置了
---
+++ deploy.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+CMD=$1
+
+case ${CMD} in
+start)
+    echo "===== start ===="
+    sudo docker-compose -f akkad.yml down
+    sudo docker-compose -f akkad.yml up -d
+    sleep 3
+    sudo docker ps -a
+    echo "===== end ===="
+    ;;
+stop)
+    echo "===== stop ===="
+    sudo docker-compose -f akkad.yml down
+    sleep 3
+    sudo docker ps -a
+    echo "===== end ===="
+    ;;
+reset)
+    echo "===== reset ===="
+    sudo docker-compose -f akkad.yml down
+    sleep 3
+    cd ./services && sudo rm -rf blockscout-db-data logs redis-data stats-db-data && cd ..
+    sudo docker-compose -f akkad.yml up -d
+    sleep 3
+    sudo docker ps -a
+    echo "===== end ===="
+    ;;
+*)
+    echo "Usage: deploy.sh start | stop | reset"
+    ;;
+esac
